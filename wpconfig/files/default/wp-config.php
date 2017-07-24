ba66d745ba3de2dfa7f7eb22ff604e29b4d7b513
Patel, Bhargav J <bhargav.j.patel@accenture.com>
2017-07-24 23:22:44 +0530

    first commit

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,10 +14,10 @@
  * @package WordPress
  */
 
-define('WP_REDIS_HOST', '');
+define('WP_REDIS_HOST', 'wordpress-001.egsyrs.0001.usw2.cache.amazonaws.com');
  
-define('WP_HOME','');
-define('WP_SITEURL','');
+define('WP_HOME','http://wordpress-1718921224.us-west-2.elb.amazonaws.com');
+define('WP_SITEURL','http://wordpress-1718921224.us-west-2.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
@@ -30,7 +30,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', '');
+define('DB_HOST', 'wordpress.cvsb9k9eqykb.us-west-2.rds.amazonaws.com:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
