906916720907875f8184b6812f059a467ec02b11
Bhargav <BJP232004@GMAIL.COM>
2017-07-25 00:18:12 +0530

    Update default.rb

--- wpconfig/recipes/default.rb
+++ wpconfig/recipes/default.rb
@@ -1,7 +1,15 @@
+group 'www-data'
+
+user 'deploy' do
+  group 'www-data'
+  system true
+  shell '/bin/bash'
+end
+
 cookbook_file "/srv/www/wordpress/current/wp-config.php" do
   source "wp-config.php"
   mode '0777'
   action :create
-  owner 'root'
-  group 'root'
+  owner 'deploy'
+  group 'www-data'
 end
